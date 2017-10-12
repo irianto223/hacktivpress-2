ed968f14231c494685172966555ab595fdfec8f2
irianto <email": null>
2017-10-12 14:20:18 +0700

    fitur login API done

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,7 +5,7 @@ var bcrypt = require('bcryptjs')
 var register = (req, res) => {
   let salt = bcrypt.genSaltSync(10);
   let hash = bcrypt.hashSync(req.body.password, salt);
-  
+
   User.create({
     username: req.body.username,
     password: hash
@@ -16,11 +16,47 @@ var register = (req, res) => {
       data: dataUser
     })
   })
-  .catch(err => {
-    res.send(err)
+  .catch(err => res.send(err))
+}
+
+var login = (req, res) => {
+  User.findOne({
+    username: req.body.username
   })
+  .then(dataUser => {
+    if (dataUser != null) {
+      let loginStatus = bcrypt.compareSync(req.body.password, dataUser.password)
+      if (loginStatus) {
+        let token = jwt.sign({
+          _id: dataUser.id,
+          username: dataUser.username
+        }, 'satekambing')
+
+        res.send({
+          message: 'login success',
+          token: token,
+          data: {
+            _id: dataUser._id,
+            username: dataUser.username
+          }
+        })
+      } else {
+        res.send({
+          message: 'password wrong',
+          data: null
+        })
+      }
+    } else {
+      res.send({
+        message: 'username not found',
+        data: dataUser
+      })
+    }
+  })
+  .catch(err => res.send(err))
 }
 
 module.exports = {
-  register
+  register,
+  login
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -8,5 +8,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 module.exports = router;
