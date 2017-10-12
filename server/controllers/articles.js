3e5bfb7c04559046a8bf0f3af7fbecc50d40d1f3
irianto <email": null>
2017-10-12 14:31:47 +0700

    checkpoint bang Tama

---
+++ server/controllers/articles.js
@@ -0,0 +1,23 @@
+var Article = require('../models/Article')
+var jwt = require('jsonwebtoken')
+
+var createArticle = (req, res) => {
+  let decoded = jwt.verify(req.headers.token, 'satekambing')
+
+  Article.create({
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    author: decoded._id
+  })
+  .then(dataArticle => {
+    res.send({
+      message: 'data created',
+      data: dataArticle
+    })
+  })
+}
+
+module.exports = {
+  createArticle
+}
--- server/models/Article.js
+++ server/models/Article.js
@@ -1,5 +1,23 @@
 var mongoose = require('mongoose')
 
 var schema = mongoose.Schema({
-  
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  category: String,
+  author: {
+    type: mongoose.Schema.Type.ObjectId,
+    ref: 'users'
+  }
+}, {
+  timestamps: true
 })
+
+var Article = mongoose.model('Article', schema)
+
+module.exports = Article
